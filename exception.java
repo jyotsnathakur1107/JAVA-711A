c14c4593693c9b8f87e1f7eb5f6303fee2daf335
Jyotsna Thakur <jyotsnathakur1107@gmail.com>
2026-02-20 13:55:14 +0530

    commit

--- exception.java
+++ exception.java
@@ -1,6 +1,6 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
-public class Marks {
+public class exception {
     public static void main (String args[]) {
         Scanner sc = new Scanner(System.in);
 
